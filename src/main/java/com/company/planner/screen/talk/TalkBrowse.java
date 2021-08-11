eeb7ec93f43d0b80a3e40e1a3f2c5f4c9408b1c2
Ronakon.wat <88375612+ronakon45@users.noreply.github.com>
2021-08-11 12:04:40 +0700

    Use Screen API

--- src/main/java/com/company/planner/screen/talk/TalkBrowse.java
+++ src/main/java/com/company/planner/screen/talk/TalkBrowse.java
@@ -1,10 +1,30 @@
 package com.company.planner.screen.talk;
 
+import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.component.Calendar;
 import io.jmix.ui.screen.*;
 import com.company.planner.entity.Talk;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 @UiController("Talk.browse")
 @UiDescriptor("talk-browse.xml")
 @LookupComponent("talksTable")
 public class TalkBrowse extends StandardLookup<Talk> {
+    @Autowired
+    private ScreenBuilders screenBuilders;
+
+    @Subscribe("talksCalendar")
+    public void onTalksCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<LocalDateTime> event) {
+        screenBuilders.editor(Talk.class, this)
+                .editEntity((Talk) event.getEntity())
+                .withOpenMode(OpenMode.DIALOG)
+                .withScreenClass(TalkEdit.class)
+                .withAfterCloseListener(afterCloseEvent -> {
+                    if (afterCloseEvent.closedWith(StandardOutcome.COMMIT)) {
+                        getScreenData().loadAll();
+                    }
+                }).show();
+    }
 }
